06cc827142610a34ec46915587ac54e061b5bc02
BanqingCSDN <40236804+BanqingCSDN@users.noreply.github.com>
2018-06-18 19:31:49 +0800

    创建提交

--- MyTestNew/app/src/main/java/test/com/mytestnew/MainActivity.java
+++ MyTestNew/app/src/main/java/test/com/mytestnew/MainActivity.java
@@ -3,11 +3,22 @@ package test.com.mytestnew;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * createBy 2018-6-18
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    /**
+     * 初始化view
+     */
+    private void initView() {
+
     }
 }
